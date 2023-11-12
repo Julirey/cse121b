ddf09f66f0e8d9e6c4969194137a53da22e2f641
julioreyes27 <reyes.0271204@gmail.com>
2023-11-12 02:09:10 -0400

    Add w02 files

---
+++ scripts/w02-task.js
@@ -0,0 +1,47 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+const fullName = "Julio Reyes";
+const currentYear = new Date().getFullYear;
+const profilePicture = "images/profile_picture.jpg";
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+currentYear.textContent = 2023;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
+
+/* Step 5 - Array */
+
+let favoriteFoods = ["Ice cream", "Hamburguer", "Spaghetti"]
+foodElement.innerHTML = favoriteFoods;
+
+let anotherFood = "Chocolate";
+favoriteFoods.push(anotherFood);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+
+
+
+
+
+
+
+
