367ad97b170d583684d527ed986b917510c5dec0
julioreyes27 <reyes.0271204@gmail.com>
2023-11-26 02:16:25 -0400

    Add w04 files

---
+++ scripts/w04-task.js
@@ -0,0 +1,63 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Julio Reyes",
+    photo: "images/profile_picture.jpg",
+    favoriteFoods: [
+        "Ice cream",
+        "Hamburguer",
+        "Spaghetti",
+        "Chocolate"
+    ],
+    hobbies: [
+        "Learning languages",
+        "Volleyball",
+        "Ping Pong",
+        "Drawing",
+        "Piano",
+        "Guitar"
+    ],
+    placesLived: []
+ };
+
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+    {
+        place: "Zulia, Venezuela",
+        length: "My whole life"
+    }
+ )
+
+/* DOM Manipulation - Output */
+
+/* Name */
+document.querySelector("#name").textContent = myProfile.name;
+
+/* Photo with attributes */
+document.querySelector('#photo').src = myProfile.photo;
+document.querySelector('#photo').alt = myProfile.name;
+
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(food => {
+    let li = document.createElement("li");
+    li.textContent = food;
+    document.querySelector("#favorite-foods").appendChild(li);
+});
+
+/* Hobbies List */
+myProfile.hobbies.forEach(hobby => {
+    let li = document.createElement("li");
+    li.textContent = hobby;
+    document.querySelector("#hobbies").appendChild(li);
+});
+
+/* Places Lived DataList */
+myProfile.placesLived.forEach(place => {
+    let dt = document.createElement("dt");
+    dt.textContent = place.place;
+    let dd = document.createElement("dd");
+    dd.textContent = place.length;
+    document.querySelector("#places-lived").appendChild(dt);
+    document.querySelector("#places-lived").appendChild(dd);
+});
